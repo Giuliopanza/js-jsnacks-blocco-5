5b1591ac191bdc072d26b39a720c0b6ab1847c99
Giulio <giuliopanza2000@gmail.com>
2025-01-27 12:51:27 +0100

    complete 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,11 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const nome = posts.map ((Element) => {
+  return Element.author 
+})
+
+console.log (`${nome}`)
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
